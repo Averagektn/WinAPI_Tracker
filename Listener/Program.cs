ea2cb31a09d5056b0d52170e7426434eee13273e
Никита Глушаченко <rylonoboev2004@gmail.com>
2023-11-23 11:10:15 +0300

    Listener implemented

---
+++ Listener/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+IPAddress ipAddress = IPAddress.Any;
+string filename = "dataset_11_all_coords.txt";
+string ip = "127.0.0.1";
+int port = 9998;
+int bytesRead;
+int c = 0;
+byte[] tmp = new byte[1];
+byte[] buffer = new byte[4];
+
+using var listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+listener.Connect(IPAddress.Parse(ip), port);
+listener.Receive(tmp);
+
+Console.WriteLine("Received: " + tmp[0].ToString());
+
+listener.Send(tmp);
+
+using var writer = new StreamWriter(filename);
+while ((bytesRead = listener.Receive(buffer)) > 0)
+{
+    byte[] data = new byte[bytesRead];
+    Array.Copy(buffer, data, bytesRead);
+    float fl = BitConverter.ToSingle(buffer, 0);
+
+    if (c != 2)
+    {
+        Console.Write(fl + " ");
+        writer.Write(fl + " ");
+        c++;
+        writer.Flush();
+    }
+    else
+    {
+        Console.WriteLine(fl + " ");
+        writer.WriteLine(fl + " ");
+        c = 0;
+        writer.Flush();
+    }
+}
