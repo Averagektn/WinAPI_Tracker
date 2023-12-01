1079b575762181200701a3709f62e256993da8bf
Никита Глушаченко <rylonoboev2004@gmail.com>
2023-12-02 00:57:08 +0300

    Axis is OK

--- src/tracker/view/header/Axis.h
+++ src/tracker/view/header/Axis.h
@@ -4,50 +4,47 @@
 #include <d2d1.h>
 
 /// <summary>
-/// 
+///		Axis implementation
 /// </summary>
 class Axis
 {
 public:
 	/// <summary>
-	/// 
+	///		Initializes axis with specified starting and ending points
 	/// </summary>
 	/// <param name="xLeft">
-	/// 
+	///		Starting point X coordinate
 	/// </param>
 	/// <param name="yLeft">
-	/// 
+	///		Starting point Y coordinate
 	/// </param>
 	/// <param name="xRight">
-	/// 
+	///		Ending point X coordinate
 	/// </param>
 	/// <param name="yRight">
-	/// 
+	///		Ending point Y coordinate
 	/// </param>
 	Axis(INT xLeft, INT yLeft, INT xRight, INT yRight);
 	/// <summary>
-	/// 
+	///		Initializes axis with specified staring and ending points
 	/// </summary>
 	/// <param name="left">
-	/// 
+	///		Starting point
 	/// </param>
 	/// <param name="right">
-	/// 
+	///		Ending point
 	/// </param>
 	Axis(POINT left, POINT right);
 
 	/// <summary>
-	/// 
+	///		Draws axis
 	/// </summary>
 	/// <param name="renderTarget">
-	/// 
+	///		Drawer
 	/// </param>
 	/// <param name="color">
-	/// 
+	///		Color of the axis
 	/// </param>
-	/// <returns>
-	/// 
-	/// </returns>
 	VOID Draw(ID2D1HwndRenderTarget* renderTarget, D2D1::ColorF color);
 private:
 	POINT left, right;
