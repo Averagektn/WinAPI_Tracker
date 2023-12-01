962e3e3c174ad8c0be06813ea8d54c8647afba4d
Никита Глушаченко <rylonoboev2004@gmail.com>
2023-12-01 23:45:57 +0300

    Logger is OK

--- src/tracker/data/header/Logger.h
+++ src/tracker/data/header/Logger.h
@@ -6,99 +6,78 @@
 #include <windows.h>
 
 /// <summary>
-/// 
+///		Logger implementation
 /// </summary>
 class Logger
 {
 public:
 	/// <summary>
-	/// 
+	///		Creates file
 	/// </summary>
 	/// <param name="filePath">
-	/// 
+	///		File to create
 	/// </param>
 	/// <param name="separator">
-	/// 
+	///		File separator
 	/// </param>
 	Logger(std::string filePath, CHAR separator);
 
 	/// <summary>
-	/// 
+	///		Closes file
 	/// </summary>
 	~Logger();
 
 	/// <summary>
-	/// 
+	///		Puts file pointer to the beginning of the file
 	/// </summary>
-	/// <returns>
-	/// 
-	/// </returns>
 	VOID Restart();
 
 	/// <summary>
-	/// 
+	///		Logs point coordinates as (POINT.X + SEPARATOR + POINT.Y + "\n")
 	/// </summary>
 	/// <param name="point">
-	/// 
+	///		Point to log
 	/// </param>
-	/// <returns>
-	/// 
-	/// </returns>
 	VOID LogLn(POINT point);
 
 	/// <summary>
-	/// 
+	///		Logs point coordinates as (POINTFLOAT.X + SEPARATOR + POINTFLOAT.Y + "\n")
 	/// </summary>
 	/// <param name="point">
-	/// 
+	///		Point to log
 	/// </param>
-	/// <returns>
-	/// 
-	/// </returns>
 	VOID LogLn(POINTFLOAT point);
 
 	/// <summary>
-	/// 
+	///		Logs string in file + "\n"
 	/// </summary>
 	/// <param name="info">
-	/// 
+	///		String to log
 	/// </param>
-	/// <returns>
-	/// 
-	/// </returns>
 	VOID LogLn(std::string info);
 
 	/// <summary>
-	/// 
+	///		Logs point coordinates as (POINT.X + SEPARATOR + POINT.Y)
 	/// </summary>
 	/// <param name="point">
-	/// 
+	///		Point to log
 	/// </param>
-	/// <returns>
-	/// 
-	/// </returns>
 	VOID Log(POINT point);
 
 	/// <summary>
-	/// 
+	///		Logs point coordinates as (POINTFLOAT.X + SEPARATOR + POINTFLOAT.Y)
 	/// </summary>
 	/// <param name="point">
-	/// 
+	///		Point to log
 	/// </param>
-	/// <returns>
-	/// 
-	/// </returns>
 	VOID Log(POINTFLOAT point);
 
 	/// <summary>
-	/// 
+	///		Logs string in file
 	/// </summary>
 	/// <param name="info">
-	/// 
+	///		String to log
 	/// </param>
-	/// <returns
-	/// 
-	/// ></returns>
 	VOID Log(std::string info);
 protected:
 	std::ofstream outputFile;
